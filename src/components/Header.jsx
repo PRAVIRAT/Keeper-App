9bd93498be02c1caca64d0530d35c40860c83297
PRAVIRAT <gmpraveen1806@gmail.com>
2025-10-27 22:01:40 +0530

    trial commit 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><defs><path id="a" d="M0 0h24v24H0V0z"></path></defs><path d="M6 14l3 3v5h6v-5l3-3V9H6zm5-12h2v3h-2zM3.5 5.875L4.914 4.46l2.12 2.122L5.62 7.997zm13.46.71l2.123-2.12 1.414 1.414L18.375 8z"></path></svg>Keeper</h1>
+      <h1><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><defs><path id="a" d="M0 0h24v24H0V0z"></path></defs><path d="M6 14l3 3v5h6v-5l3-3V9H6zm5-12h2v3h-2zM3.5 5.875L4.914 4.46l2.12 2.122L5.62 7.997zm13.46.71l2.123-2.12 1.414 1.414L18.375 8z"></path></svg>Hooper</h1>
     </header>
   );
 }
